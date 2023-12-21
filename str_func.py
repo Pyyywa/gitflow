1b726a925cec9cc38251294f3988dbee91c10a84
Rusha <pyyywa@gmail.com>
2023-12-21 16:53:10 +0300

    add func

--- str_func.py
+++ str_func.py
@@ -1,5 +1,12 @@
 def func(word):
+    "Функция делает все буквы заглавными"
     return word.upper()
+
+def func2(word):
+    "Функция делает заглавными первые буквы слова"
+    return word.title()
+
 
 line = input('Введите строку')
 print(func(line))
+print(func2(line))
